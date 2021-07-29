c89713cb9bd6876b28ad8cd439b6d474a8cf2c35
mak7imyss <mak7imyss@mail.ru>
2021-07-29 15:09:47 +0300

    refactor: updating getLikesRating and getDislikesRating

--- src/api/likesApi.ts
+++ src/api/likesApi.ts
@@ -1,34 +1,43 @@
 import got from 'got';
 
-const addLikes = async (id: number, data: {like: boolean; dislike: boolean}) => {
+const addLikes = async (id: number, data: { like: boolean; dislike: boolean }) => {
   const response = await got.post(`https://meowle.qa-fintech.ru/api/likes/cats/${id}/likes`, {
     json: data,
     responseType: 'json',
   });
   return response.body;
-}
+};
 
 const getLikesRating = async () => {
   const response = await got.get('https://meowle.qa-fintech.ru/api/likes/cats/likes-rating');
-  return response.body;
-}
+  let likes: number[] = [];
+  const resp = response.body.split(/({"\w+":\d+,"\D+)/gmi);
+  for (let i = 2; i < resp.length; i += 2) {
+    likes.push(Number(resp[i].split('}')[0]));
+  }
+  return likes;
+};
 
 const getDisikesRating = async () => {
   const response = await got.get('https://meowle.qa-fintech.ru/api/likes/cats/dislikes-rating');
-  return response.body;
-}
+  let dislikes: number[] = [];
+  const resp = response.body.split(/({"\w+":\d+,"\D+)/gmi);
+  for (let i = 2; i < resp.length; i += 2) {
+    dislikes.push(Number(resp[i].split('}')[0]));
+  }
+  return dislikes;
+};
 
 const getRating = async () => {
   const response = await got.get('https://meowle.qa-fintech.ru/api/likes/cats/rating');
   return response.body;
-}
-
+};
 
 
 const api = {
   addLikes,
   getDisikesRating,
   getLikesRating,
-  getRating
+  getRating,
 };
 export default api;
